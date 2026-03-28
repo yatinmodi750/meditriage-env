435f3f2ee7717c08ee4e1c3d28f72a4c56a6f17d
Yatinmodi750 <yatinmodi1975@gmail.com>
2026-03-28 18:40:57 +0530

    deleted bat file

--- run.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-set HF_TOKEN=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
-set MODEL_NAME=mistralai/Mistral-7B-Instruct-v0.3
-set API_BASE_URL=https://router.huggingface.co/v1
-python inference.py
