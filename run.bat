42570fcfd1d02c7eade14037ae04bbde4466b4d4
Yatinmodi750 <yatinmodi1975@gmail.com>
2026-03-28 18:39:41 +0530

    Inference py changes

---
+++ run.bat
@@ -0,0 +1,5 @@
+@echo off
+set HF_TOKEN=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
+set MODEL_NAME=mistralai/Mistral-7B-Instruct-v0.3
+set API_BASE_URL=https://router.huggingface.co/v1
+python inference.py
